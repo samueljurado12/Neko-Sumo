36f80fa828114439453a1f803c01c1b4b6437185
vistaero <vistaero@outlook.com>
2019-07-26 20:09:49 +0200

    Menú principal
    Falta el arte

--- Assets/_Scripts/MainMenuScript.cs
+++ Assets/_Scripts/MainMenuScript.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField]
+    Button[] buttons;
+
+    private void Start()
     {
-        
+        if (Input.GetJoystickNames().Length > 0);
+            buttons[0].Select();
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+            buttons[0].Select();
+    }
+    
+    public void StartGameButton()
+    {
+        ScenesManager.Instance.LoadScene(1);
     }
 }
---
+++ Assets/_Scripts/ScenesManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ScenesManager : MonoBehaviour
+{
+    public static ScenesManager Instance { private set; get; }
+
+    [SerializeField]
+    Image fadeGraphic;
+
+    private void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+    }
+
+    public void LoadScene(int index)
+    {
+        fadeGraphic.DOFade(1, 1f).OnComplete(() => SceneManager.LoadScene(index));
+    }
+
+    private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
+    {
+        fadeGraphic.DOFade(0, 1);
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
