ef1574e1ba9747c8aa7da0bfb66cc4a545ceb6ad
vistaero <vistaero@outlook.com>
2019-07-27 21:02:53 +0200

    Fricción apañada y escena definitiva

--- Assets/Scripts/KeepUp.cs
+++ Assets/Scripts/KeepUp.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class KeepUp : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    CharacterMovement character;
+    Rigidbody2D rb;
+
+    private void Awake()
     {
-        
+        character = GetComponent<CharacterMovement>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.up = PlatformManager.Instance.transform.up;
+        if (character.GetGrounded())
+            rb.drag = 5;
+        else
+            rb.drag = 0.2f;
+
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        rb.drag = 1;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        rb.drag = 0.2f;
     }
 }
