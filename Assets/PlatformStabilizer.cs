0d218cdf4c78a89dbcc349928b9ab1d24b3a4afc
vistaero <vistaero@outlook.com>
2019-07-27 11:33:50 +0200

    PID

---
+++ Assets/PlatformStabilizer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformStabilizer : MonoBehaviour
+{
+    public PID pid;
+    Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rigidbody.AddTorque(pid.Update(0, transform.localRotation.z, Time.deltaTime));
+    }
+}
