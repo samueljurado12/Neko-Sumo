48b8b5b9d226f8c8dc6294c1f4c47e0ce966caa4
vistaero <vistaero@outlook.com>
2019-07-27 18:31:51 +0200

    Pantalla ganar

---
+++ Assets/Scripts/OffScreen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OffScreen : MonoBehaviour
+{
+    bool victory;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && victory == false)
+        {
+            int player = other.GetComponent<CharacterMovement>().PlayerNumber;
+            if (player == 1)
+                WinScreen.Instance.Show(2);
+            else if (player == 2)
+                WinScreen.Instance.Show(1);
+            victory = true;
+
+        }else if (other.GetComponent<Furball>())
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
